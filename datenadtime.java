004a77317c9adbead9c56a1b0352b3012cc7c804
AidenXu6 <1548293@learn.vsb.bc.ca>
2026-09-14 14:54:35 -0700

    Add files via upload

---
+++ datenadtime.java
@@ -0,0 +1,16 @@
+public class date{
+	
+	public static void main(String[] args){
+			String day = "Monday";
+			int date = 14;
+			String month = "September";
+			int year = 2026;
+			int hours=14;
+			int minutes=42;
+			int secmid=(hours*3600)+(minutes*60);
+			System.out.println("The date of today is: " + day + ", " + month  +" "+  date + ", " + year);
+			System.out.println("");
+			System.out.print("Seconds since midnight at 2:42pm: "+secmid);
+		
+		}
+}
